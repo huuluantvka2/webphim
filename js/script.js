1fdde3f5f50052a3db396b811007e395e6dcdbea
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-04-25 21:34:37 +0700

    hi

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function () {
     $('.carousel').carousel({
         interval: 2000
     })
-    $(window).scroll(function(event){
-        var pos_body = $('html,body').scrollTop();
-        if(pos_body >201){
-            $('.return-top').addClass('active-return');
-        }else{
-            $('.return-top').removeClass('active-return');
-        }
-    });
+    // $(window).scroll(function(event){
+    //     var pos_body = $('html,body').scrollTop();
+    //     if(pos_body >201){
+    //         $('.return-top').addClass('active-return');
+    //     }else{
+    //         $('.return-top').removeClass('active-return');
+    //     }
+    // });
 });
