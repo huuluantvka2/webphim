95515f23e3f4338ab3e08bc542e3988f6a3a8364
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-04-25 11:00:29 +0700

    nop phan content

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.carousel').carousel({
+        interval: 2000
+    })
+    $(window).scroll(function(event){
+        var pos_body = $('html,body').scrollTop();
+        if(pos_body >201){
+            $('.return-top').addClass('active-return');
+        }else{
+            $('.return-top').removeClass('active-return');
+        }
+    });
+});
